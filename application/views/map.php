23b2939f3adba774e17a1459a20dcbf557fe1e71
Xavier Bertels <git@xavierbertels.com>
2012-03-25 18:21:57 +0200

    restructure markup

--- application/views/map.php
+++ application/views/map.php
@@ -2,51 +2,46 @@
 <html>
 <head>
 	<title>Ghendetta</title>
-	<meta charset="utf-8">
-	
-	<link rel="stylesheet" href="css/1140.css" media="screen" />
+	<meta charset="utf-8" />
 	<link rel="stylesheet" href="css/styles.css" media="screen" />
 	<script src="js/css3-mediaqueries.js"></script>
-
+	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
+<section class="main" role="main">
+	<h1><img src="img/logo.png" class="logo" alt="Ghendetta" /></h1>
+	<h2>Welcome!</h2>
 
-<div id="sidebar">
-	<img src="img/logo.png" id="logo">
-	
-	<div id="login">
-		<p id="welcome">Welcome, Godfather!</p>
-	</div>
-	
-	<?php if(!$this->ghendetta->current_user()): ?>
-		<a href="<?php echo site_url('foursquare/auth'); ?>"><img src="img/connect-white.png" id="foursquare"></a>
-	<?php else: ?>
+	<p class="message warning">Warning: enter the game at your own risk. It is very much &szlig;. Lives or data will be lost.</p>
+
+	<p>
+		<?php if(!$this->ghendetta->current_user()): ?>
+		<a href="<?php echo site_url('foursquare/auth'); ?>"><img src="img/connect-white.png" class="foursquare" /></a>
+		<?php else: ?>
 		<a href="#" class="button">Manage your Clan</a>
-	<?php endif; ?>
-	
-	<div id="leadingclans">
-		<ul>
-			<li><img src="img/wapenschild4.png">Turtles</li>
-			<li><img src="img/wapenschild2.png">iRail</li>
-			<li><img src="img/wapenschild3.png">GhentMob</li>
-			<li><img src="img/wapenschild1.png">DriveBy</li>
-		</ul>
-	</div>
-</div>
+		<?php endif; ?>
+	</p>
 
-<div id="map_canvas"></div>
+	<ul class="leaderboard">
+		<li><img src="img/wapenschild4.png" />Turtles</li>
+		<li><img src="img/wapenschild2.png" />iRail</li>
+		<li><img src="img/wapenschild3.png" />GhentMob</li>
+		<li><img src="img/wapenschild1.png" />DriveBy</li>
+	</ul>
+</section>
+
+<section id="map_canvas"></section>
 
 <script src="js/jquery.js"></script>
 <script src="//maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script src="js/application.js"></script>
 <script src="js/battlefield.js"></script>
 <script>
-var battlefield = <?php echo json_encode($battlefield); ?>;
-
-$(document).ready(function() {
-    visualize(battlefield);
-});
+	var battlefield = <?php echo json_encode($battlefield); ?>;
+	$(document).ready(function()
+	{
+	    visualize(battlefield);
+	});
 </script>
-
 </body>
 </html>
