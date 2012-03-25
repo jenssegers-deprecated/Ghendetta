2e26ad0e7becbe8d1d1f120bfdaeda73ae412ee2
Jens Segers <segers.jens@gmail.com>
2012-03-25 19:43:28 +0200

    Use same cookies for www. and without

--- application/config/config.php
+++ application/config/config.php
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "";
-$config['cookie_domain']	= "";
+$config['cookie_domain']	= "ghendetta.be";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
