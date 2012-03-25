88105a296970a05c49a914d3a4f2770b08a4a21f
Xavier Bertels <git@xavierbertels.com>
2012-03-25 19:14:18 +0200

    put disclaimer under foursquare button

--- application/views/map.php
+++ application/views/map.php
@@ -12,10 +12,6 @@
 	<h1><img src="img/logo.png" class="logo" alt="Ghendetta" /></h1>
 	<h2>Welcome!</h2>
 
-	<div class="message warning">
-		<p><strong>Warning:</strong> enter at your own risk. It is very much &szlig;. Lives or game data will be lost. <small>But we respect your privacy, and your Foursquare data is safe!</small></p>
-	</div>
-
 	<p>
 		<?php if(!$this->ghendetta->current_user()): ?>
 		<a href="<?php echo site_url('foursquare/auth'); ?>"><img src="img/connect-white.png" class="foursquare" /></a>
@@ -23,6 +19,10 @@
 		Jouw clan: <strong style="color:#<?php echo $clan['color']; ?>"><?php echo $clan['name']; ?></strong>
 		<?php endif; ?>
 	</p>
+
+	<div class="message warning">
+		<p><strong>Warning:</strong> enter at your own risk. It is very much &szlig;. Lives or game data will be lost. <small>But we respect your privacy, and your Foursquare data is safe!</small></p>
+	</div>
 
 	<ul class="leaderboard">
 		<?php foreach($clans as $clan): ?>
