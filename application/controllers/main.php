0e4ebea3c3e6e2d0aa2cbf26755e67eb03b8e797
Jens Segers <segers.jens@gmail.com>
2012-03-25 15:06:39 +0200

    Renamed default controller

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -2,7 +2,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Main extends CI_Controller {
     
     public function index() {
         $this->load->model('region_model');
