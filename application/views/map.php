1f7072748793af95ada6fa5915e8b6da8397b0e0
Xavier Bertels <git@xavierbertels.com>
2012-03-25 20:10:46 +0200

    restructure markup for clan name

--- application/views/map.php
+++ application/views/map.php
@@ -12,13 +12,11 @@
 	<h1><img src="img/logo.png" class="logo" alt="Ghendetta" /></h1>
 	<h2>Welcome!</h2>
 
-	<p>
 		<?php if(!$this->ghendetta->current_user()): ?>
-		<a href="<?php echo site_url('foursquare/auth'); ?>"><img src="img/connect-white.png" class="foursquare" /></a>
+		<p class="foursquare"><a href="<?php echo site_url('foursquare/auth'); ?>"><img src="img/connect-white.png" /></a></p>
 		<?php else: ?>
-		Jouw clan: <strong style="color:#<?php echo $clan['color']; ?>"><?php echo $clan['name']; ?></strong>
+		<p class="clan">Your clan: <strong style="background:#<?php echo $clan['color']; ?>"><?php echo $clan['name']; ?></strong></p>
 		<?php endif; ?>
-	</p>
 
 	<div class="message warning">
 		<p><strong>Warning:</strong> enter at your own risk. It is very much &szlig;. Lives or game data will be lost. <small>But we respect your privacy, and your Foursquare data is safe!</small></p>
