9b625fc4bf3fdbeba27ff32d305fc9e8de263042
Xavier Bertels <git@xavierbertels.com>
2012-03-27 23:48:09 +0200

    add your baatles icon and legend

--- application/views/map.php
+++ application/views/map.php
@@ -25,18 +25,22 @@
 		<h2>Welcome to Ghendetta</h2>
 		<p class="clan">You are with the <strong style="background:#<?php echo $clan['color']; ?>"><?php echo $clan['name']; ?></strong> clan.</p>
 		<div class="tutorial">
+			<dl class="legend cf">
+				<dt><img src="/img/ico_battle.png" /></dt>
+				<dd>Your battles</dd>
+			</dl>
 			<h2>How To Play</h2>
 			<p>Every visit to another district (check-in on Foursquare) gives you a chance to take away a district from a rivaling clan.</p>
 		</div>
 		<?php endif; ?>
-		
+
 		<!-- <p class="tutorial" >
 			<a href="https://twitter.com/Ghendetta" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @Ghendetta</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;
 						  js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 			</script> us on twitter for updates about new features.
 		</p> -->
-		
+
 	<ul class="leaderboard">
 		<?php foreach($clans as $clan): ?>
 		<li><img src="<?php echo $clan['logo']; ?>"><?php echo $clan['name']; ?></li>
