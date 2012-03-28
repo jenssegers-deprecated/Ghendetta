f0bd865ec5e5edabc9178dd5aa96b8f0fbcd4fef
Xavier Bertels <git@xavierbertels.com>
2012-03-29 00:58:48 +0200

    make progress bar site-wide

--- application/views/clan-waiting.php
+++ application/views/clan-waiting.php
@@ -1,8 +1,10 @@
 <?php include_once('head.tpl'); ?>
 <body>
 <?php include_once('navigation.tpl'); ?>
-<section class="main" role="main">
+<section class="clan" role="main">
+	<h1>Clan Page</h1>
 	<p>Almost there!</p>
+	<p class="pr-cont"><span class="pr" style="width:70%"></span></p>
 </section>
 
 </body>
