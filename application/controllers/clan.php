a023092dbd300a463f92950001b9a8c7a25628cb
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-03-29 01:57:24 +0200

    move debug to clan

--- application/controllers/clan.php
+++ application/controllers/clan.php
@@ -5,19 +5,31 @@ if (!defined('BASEPATH'))
 class Clan extends CI_Controller {
     
     function index() {
-        $this->load->model('clan_model');
-        
+        // this is no controller where unregistered users are allowed to hang out
         $user = $this->ghendetta->current_user();
         if (!$user) {
             redirect();
         }
+
+        // load model & clan
+        $this->load->model('clan_model');
+        $clan = $this->clan_model->get($user['clanid']);
         
+        // calculate the progress, start at 60 to start, and end with 100 at 23:15
+        $now = time() ;
+        $release = 1333055771 ; // 23:15
+        $start   = 1332976478 ;   // 01:15 ofzo
+        
+        $progress = ($now - $start) * 1.0 / ($release - $start) ;
+        $progress = floor( 60 + ( $progress * 40 )) ;
+        
+        // setting data
         $data = array();
-        $data['user'] = $user;
-        $data['clan'] = $this->clan_model->get($user['clanid']);
-        $data['clanmembers'] = $this->clan_model->get_members($user['clanid']);
-        
-        $this->load->view('clan', $data);
+        $data['progress'] = $progress ;
+        $data['clan'] = $clan ;
+
+        // view
+        $this->load->view('clan-waiting', $data);
     }
     
     function save() {
--- application/controllers/debug.php
+++ application/controllers/debug.php
@@ -2,35 +2,6 @@
 
 class debug extends CI_Controller{
 
-    function view( $view ){
-        // this is no controller where unregistered users are allowed to hang out
-        $user = $this->ghendetta->current_user();
-        if (!$user) {
-            redirect();
-        }
-
-        // load model & clan
-        $this->load->model('clan_model');
-        $clan = $this->clan_model->get($user['clanid']);
-        
-        print_r( $clan );
-        // calculate the progress, start at 60 to start, and end with 100 at 23:15
-        $now = time() ;
-        $release = 1333055771 ; // 23:15
-        $start   = 1332976478 ;   // 01:15 ofzo
-        
-        $progress = ($now - $start) * 1.0 / ($release - $start) ;
-        $progress = floor( 60 + ( $progress * 40 )) ;
-        
-        // setting data
-        $data = array();
-        $data['progress'] = $progress ;
-        $data['clan'] = $clan ;
-
-        // view
-        $this->load->view($view, $data);
-    }
-
     function index(){
         redirect();
     }
