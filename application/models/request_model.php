915ebf8887df6ddab07f9b89e1a471282535d8b4
Jens Segers <segers.jens@gmail.com>
2012-03-28 23:32:24 +0200

    Requets API

--- application/models/request_model.php
+++ application/models/request_model.php
@@ -2,6 +2,20 @@
 
 class request_model extends CI_Model {
     
+    function get_daily() {
+        $query = "
+        	SELECT FROM_UNIXTIME(time, GET_FORMAT(DATE,'EUR')) as date, time as timestamp, count(1) as requests
+        	FROM requests
+        	WHERE time >= UNIX_TIMESTAMP(subdate(now(),30))
+        	GROUP BY FROM_UNIXTIME(time, GET_FORMAT(DATE,'EUR'))";
+        
+        return $this->db->query($query)->result_array();
+    }
+    
+    function get_all() {
+        return $this->db->get('requests')->result_array();
+    }
+    
     function insert($request) {
         $this->db->insert('requests', $request);
         return $this->db->insert_id();
