0cb52d0f6aeb6d408a53f1fb1fbd703fa4642faa
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-03-29 01:47:20 +0200

    almost

--- application/views/clan-waiting.php
+++ application/views/clan-waiting.php
@@ -2,10 +2,10 @@
 <body>
 <?php include_once('navigation.tpl'); ?>
 <section class="clan" role="main">
-	<h1>My Clan</h1>
-	<p>You and your fellow associates have fought <strong>230</strong> <abbr title="Foursquare Checkins">battles</abbr>.</p>
+	<h1><?php echo $clan['name'] ?></h1>
+	<p>You and your fellow associates have fought <strong><?php echo $clan['name'] ?></strong> <abbr title="Foursquare Checkins">battles</abbr>.</p>
 	<p>Your capo is not pleased. Fight more battles to reveal who is in your clan.</p>
-	<p class="pr-cont"><span class="pr" style="width:70%"></span></p>
+	<p class="pr-cont"><span class="pr" style="width:<?= $progress ?>%"></span></p>
 </section>
 
 </body>
