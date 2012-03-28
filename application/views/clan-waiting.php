c51e95acee4fbaacfdd566ae3ec5c6d503093f12
Xavier Bertels <git@xavierbertels.com>
2012-03-29 01:14:05 +0200

    clan-waiting: add basic markup

--- application/views/clan-waiting.php
+++ application/views/clan-waiting.php
@@ -2,8 +2,9 @@
 <body>
 <?php include_once('navigation.tpl'); ?>
 <section class="clan" role="main">
-	<h1>Clan Page</h1>
-	<p>Almost there!</p>
+	<h1>My Clan</h1>
+	<p>You and your fellow associates have fought <strong>230</strong> <abbr title="Foursquare Checkins">battles</abbr>.</p>
+	<p>Your capo is not pleased. Fight more battles to reveal who is in your clan.</p>
 	<p class="pr-cont"><span class="pr" style="width:70%"></span></p>
 </section>
 
