7a7e39ab5b44e0fdda766bc9731bd4c512ffc298
Xavier Bertels <git@xavierbertels.com>
2012-03-29 02:14:02 +0200

    add My clan

--- application/views/clan-waiting.php
+++ application/views/clan-waiting.php
@@ -2,7 +2,7 @@
 <body>
 <?php include_once('navigation.tpl'); ?>
 <section class="clan" role="main">
-	<h1><?php echo  $clan['name'] ?></h1>
+	<h1>My Clan: <?php echo  $clan['name'] ?></h1>
 	<p>You and your fellow associates have fought <strong><?php echo  $clan['score'] ?></strong> <abbr title="Foursquare Checkins">battles</abbr>.</p>
 	<p>Your capo is not pleased. Fight more battles to reveal who is in your clan.</p>
 	<p class="pr-cont"><span class="pr" style="width:<?php echo  $progress ?>%"></span></p>
