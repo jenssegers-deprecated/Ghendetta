4d67be4f05a4fafee22eed068d720d5138fbf616
Xavier Bertels <git@xavierbertels.com>
2012-03-29 22:23:18 +0200

    add clan page styling

--- application/views/clan.php
+++ application/views/clan.php
@@ -1,9 +1,25 @@
-<ul>
+<?php include_once('head.tpl'); ?>
+<body>
+<?php include_once('navigation.tpl'); ?>
+<section class="clan" role="main">
+	<h1>My Clan: <?php echo $clan['name']; ?></h1>
+	<p>Congratulations. You and your fellow associates have fought <strong><?php echo  $clan['points']; ?></strong> <abbr title="Foursquare Checkins">battles</abbr>. Your Capo is pleased.</p>
 	<?php foreach($members as $member): ?>
-		<li><a href="https://foursquare.com/user/<?php echo $member['fsqid']; ?>">
-			<?php echo $member['firstname']; ?>&nbsp;<?php echo $member['lastname']; ?>
-			has done <?php echo $member['points']; ?> checkins for the <?php echo $clan['name']; ?> clan, in the last week.
-			<img src="<?php echo $member['picurl']; ?>" /></a>
-		</li>
+	<article>
+		<div class="cf">
+			<h1><a href="https://foursquare.com/user/<?php echo $member['fsqid']; ?>"><?php echo $member['firstname']; ?></a></h1>
+			<a href="https://foursquare.com/user/<?php echo $member['fsqid']; ?>">
+				<img src="<?php echo $member['picurl']; ?>" alt="<?php echo $member['firstname']; ?>" width="110" height="110" />
+			</a>
+			<dl>
+				<dt>Battles:</dt>
+				<dd class="battles"><?php echo $member['points']; ?></dd>
+				<dt>Rank:</dt>
+				<dd class="rank">Associate</dd>
+			</dl>
+		</div>
+	</article>
 	<?php endforeach; ?>
-</ul>
+</section>
+</body>
+</html>
