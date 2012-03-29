35d943830de5ac9feefd9e89023402e139607d88
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-03-30 00:52:19 +0200

    better view

--- application/views/construction.php
+++ application/views/construction.php
@@ -14,9 +14,9 @@ if (isset($offset)) {
 
 <section class="clan" role="main">
 	<h1>My Clan: <?php echo $clan['name']; ?></h1>
-	<p>You and your fellow associates have fought <strong><?php echo  $clan['points']; ?></strong> <abbr title="Foursquare Checkins">battles</abbr>.</p>
-	<p>Your capo is not pleased. Fight more battles to reveal <?php echo $feature; ?>.</p>
-	<p class="pr-cont"><span class="pr" style="width:<?php echo $progress; ?>%"></span></p>
+	<p>Your capo is excited. He wants you to fight <strong>666</strong> battles to reveal <?php echo $feature; ?>.</p>
+    <p>You and your fellow associates need to fight <strong><?php echo  666 - $clan['points']; ?></strong> more <abbr title="Foursquare Checkins">battles</abbr>.</p>	
+    <p class="pr-cont"><span class="pr" style="width:<?php echo $progress; ?>%"></span></p>
 </section>
 
 </body>
