5a3f9e5792dfff7ad74466251a9ce26b48365b5d
Jens Segers <segers.jens@gmail.com>
2012-03-31 13:02:12 +0200

    About controller ready

--- application/controllers/about.php
+++ application/controllers/about.php
@@ -12,12 +12,7 @@ if (!defined('BASEPATH'))
 class About extends CI_Controller {
     
     function index() {
-        
-        if (time() < strtotime('31.03.2012 20:00')) {
-            redirect('construction');
-        } else {
-            $this->load->view('about');
-        }
+        $this->load->view('about');
     }
 
 }
