7d646b01dbb8e78bd5fbc474771cce6288c17b9f
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-04-01 00:10:40 +0200

    map controller added

---
+++ application/controllers/map.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright (C) 2012 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jens Segers <jens at iRail.be>
+ * @author Hannes Van De Vreken <hannes at iRail.be>
+ */
+class map extends CI_Controller{
+
+    function index(){
+        this->load->view('map');    
+    }
+
+}
+
+?>
