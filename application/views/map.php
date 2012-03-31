6129845744ec158e667bf489d88f061da952e482
Xavier Bertels <git@xavierbertels.com>
2012-04-01 01:08:36 +0200

    add map view

---
+++ application/views/map.php
@@ -0,0 +1,18 @@
+<?php include_once('head.tpl'); ?>
+<body>
+<link rel="stylesheet" href="<?php echo static_url('css/mapbox/leaflet.css'); ?>">
+<?php include_once('navigation.tpl'); ?>
+
+<section id="map"></section>
+
+<script>
+	var base_url = '<?php echo base_url(); ?>';
+	var site_url = '<?php echo site_url(); ?>';
+	var static_url = '<?php echo static_url(); ?>';
+</script>
+
+<script src="<?php echo static_url('js/jquery.js'); ?>"></script>
+<script src="<?php echo static_url('js/mapbox.min.js?v=310301'); ?>"></script>
+
+</body>
+</html>
