e4cdc92533d7af3c22e10d86ec438613df5933c6
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-03-31 19:51:17 +0200

    logout finished

---
+++ application/controllers/logout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright (C) 2012 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jens Segers <jens at iRail.be>
+ * @author Hannes Van De Vreken <hannes at iRail.be>
+ */
+class logout extends CI_Controller{
+
+    function index(){
+        
+        if ( $user = $this->ghendetta->current_user() ) {
+            $this->ghendetta->unset_user();
+        }
+        
+        redirect();
+    }
+
+}
