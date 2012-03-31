a753243b7dba1da28c252ff5d0baeace917e1c22
Jens Segers <segers.jens@gmail.com>
2012-03-31 11:32:26 +0200

    Added iRail license to files

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @copyright (C) 2012 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jens Segers <jens at iRail.be>
+ * @author Hannes Van De Vreken <hannes at iRail.be>
+ */
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
     
