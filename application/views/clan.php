ce29da61afd8c0713f4a99665b301eb3ab154985
Jens Segers <segers.jens@gmail.com>
2012-04-02 00:25:57 +0200

    Clan page uses battles instead of points

--- application/views/clan.php
+++ application/views/clan.php
@@ -4,7 +4,7 @@
 
 <section class="v-clan cf" role="main">
 	<div class="bd">
-		<h1>My Clan: <?php echo $clan['name']; ?> (<strong><?php echo  $clan['points']; ?></strong> <abbr title="Foursquare Checkins">battles</abbr>)</h1>
+		<h1>My Clan: <?php echo $clan['name']; ?> (<strong><?php echo  $clan['battles']; ?></strong> <abbr title="Foursquare Checkins">battles</abbr>)</h1>
 		<p>Congratulations! Your collaborative battle spree made you unlock the clan page. Your Capo is pleased. <a href="https://twitter.com/intent/tweet?source=webclient&amp;text=Mayhem%21+%23<?php echo $clan['name']; ?>+have+just+unlocked+the+clan+page+on+http%3A%2F%2Fghendetta.be" class="button">Tweet about this.</a></p>
 	</div>
 	<?php foreach($members as $member): ?>
@@ -16,7 +16,7 @@
 			</a>
 			<dl>
 				<dt>Battles:</dt>
-				<dd class="battles"><?php echo $member['points']; ?></dd>
+				<dd class="battles"><?php echo $member['battles']; ?></dd>
 				<dt>Rank:</dt>
 				<dd class="rank">
 					<?php
