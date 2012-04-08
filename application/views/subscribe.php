1fe166feb7b38ba88a84a75413f8ffd870659676
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-04-08 22:22:41 +0200

    subscribe page

---
+++ application/views/subscribe.php
@@ -0,0 +1,16 @@
+<?php include_once('head.tpl'); ?>
+<body>
+<?php include_once('navigation.tpl'); ?>
+
+<section class="container v-warning" role="main">
+    <h1>Foursquare Day Ghent</h1>
+    <p>We are announcing Foursquare Day for Ghent. This event will take place on the 16th of april in the city center of Ghent. We are planning a special game with small assignments near predefined venues. To be able predict how much of you will be attending this event, we would like you to RSVP this invitation.</p>
+    <a href="/api/confirm">I will attend!</a><!--ajax this request! -->
+    <a href="/">No, get me out of here.</a>
+    <a>Thanks for your confirmation (--show this when attending--)</a>
+</section>
+
+<?php include_once('footer.tpl'); ?>
+
+</body>
+</html>
