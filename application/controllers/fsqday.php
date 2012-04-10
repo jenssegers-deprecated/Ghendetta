81544feb1385cbce2e26bc58d74b521ebef25430
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-04-10 22:04:58 +0200

    added fsq day view

---
+++ application/controllers/fsqday.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright (C) 2012 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jens Segers <jens at iRail.be>
+ * @author Hannes Van De Vreken <hannes at iRail.be>
+ */
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class fsqday extends CI_Controller {
+    
+    function index() {
+        $this->load->view('fsqday');
+    }
+
+}
+
+?>
