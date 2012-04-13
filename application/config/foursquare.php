e64ec5973d723468a0335ce315dadace72eb3c7d
Jens Segers <segers.jens@gmail.com>
2012-04-13 17:59:31 +0200

    Removed cronjob code

--- application/config/foursquare.php
+++ application/config/foursquare.php
@@ -6,11 +6,9 @@
 |
 | Your Foursquare application consumer credentials
 |
-| ['cronjob_code'] This is a special code that is required to call the cronjob controller
 */
 
 $config['client_id']     = '';
 $config['client_secret'] = '';
 $config['callback_url']  = '';
 $config['push_secret']   = '';
-$config['cronjob_code']  = '';
