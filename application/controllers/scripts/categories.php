89ddb20714fe413ff35fd4ca2c0db167a63d6ebf
Jens Segers <segers.jens@gmail.com>
2012-04-14 13:16:05 +0200

    CLI only scripts

---
+++ application/controllers/scripts/categories.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @copyright (C) 2012 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jens Segers <jens at iRail.be>
+ * @author Hannes Van De Vreken <hannes at iRail.be>
+ */
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Categories extends CI_Controller {
+    
+    function __construct() {
+        if (!$this->input->is_cli_request()) {
+            show_error('Scripts can only be executed from CLI');
+        }
+    }
+    
+    function index() {
+        $json = $this->foursquare->api('venues/categories');
+        
+        $this->load->model('category_model');
+        $this->category_model->truncate();
+        
+        foreach ($json->response->categories as $category) {
+            $this->process_category($category);
+        }
+    }
+    
+    private function process_category($category, $parent = '') {
+        $data = array();
+        $data['categoryid'] = $category->id;
+        $data['name'] = $category->name;
+        $data['icon'] = $category->icon;
+        $data['parent'] = $parent;
+        
+        $this->category_model->insert($data);
+        
+        // recursive child categories
+        if (isset($category->categories) && count($category->categories)) {
+            foreach ($category->categories as $subcategory) {
+                $this->process_category($subcategory, $category->id);
+            }
+        }
+    }
+}
---
+++ application/controllers/scripts/coordinates.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @copyright (C) 2012 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jens Segers <jens at iRail.be>
+ * @author Hannes Van De Vreken <hannes at iRail.be>
+ */
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Coordinates extends CI_Controller {
+    
+    function __construct() {
+        if (!$this->input->is_cli_request()) {
+            show_error('Scripts can only be executed from CLI');
+        }
+    }
+    
+    function index() {
+        $url = 'http://data.appsforghent.be/poi/kotzones.json';
+        $json = $this->datatank($url);
+        
+        $this->load->model('region_model');
+        $this->region_model->truncate();
+        
+        foreach ($json->kotzones as $kotzone) {
+            $regionid = $this->region_model->insert_region(array('name' => $kotzone->kotzone_na));
+            
+            // get coordinates
+            preg_match_all('#([0-9]+\.[0-9]+),([0-9]+\.[0-9]+)#', $kotzone->coords, $matches);
+            
+            // put coordinates in array
+            $coords = array();
+            foreach ($matches[0] as $key => $match) {
+                $coords = array('regionid' => $regionid, 'lon' => $matches[2][$key], 'lat' => $matches[1][$key]);
+                $this->region_model->insert_roords($coords);
+            }
+        }
+    }
+    
+    private function datatank($url) {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HTTPGET, TRUE);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        
+        $data = curl_exec($curl);
+        curl_close($curl);
+        
+        return json_decode($data);
+    }
+}
