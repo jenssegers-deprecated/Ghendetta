3e13cb03ffc18075483cea8141b52fa49df932a7
Jens Segers <segers.jens@gmail.com>
2012-04-22 14:25:57 +0200

    Cachebreaker

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -51,7 +51,7 @@
 
 <?php include_once('footer.tpl'); ?>
 
-<script src="<?php echo static_url('js/mapbox.min.js?v=160402'); ?>"></script>
+<script src="<?php echo static_url('js/mapbox.min.js?v=200401'); ?>"></script>
 
 </body>
 </html>
